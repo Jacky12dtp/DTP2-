e1301e0281ca60cf13bebe5f528e94b475572343
Jacky Zheng <105019522+Jacky12dtp@users.noreply.github.com>
2022-06-01 22:32:45 +0000

    added if and else statements

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ from random import randint
 player = input('rock (r), paper (P) or scissors (s)?')
 print(player,'vs')
 chosen = randint (1,3)
+print(chosen)
+
+if chosen == 1:
+  computer = 'r'
+elif chosen == 2:
+  computer = 'p'
+else :
+  computer = 's'
